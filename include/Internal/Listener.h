62af952b6eb3b25f277b1010459e62b4860e64a9
Scott <smasherprog@gmail.com>
2018-03-04 18:00:05 -0800

    more cleanup of the api

--- include/Internal/Listener.h
+++ include/Internal/Listener.h
@@ -7,12 +7,15 @@ namespace NET {
     class Socket;
     class Listener final : public IListener {
       public:
+#ifdef WIN32
         LPFN_ACCEPTEX AcceptEx_ = nullptr;
         char Buffer[(sizeof(SOCKADDR_STORAGE) + 16) * 2];
+#endif //  WIN32
+
         std::shared_ptr<Socket> ListenSocket;
         Context *Context_;
         Win_IO_Accept_Context Win_IO_Accept_Context_;
-        sockaddr ListenSocketAddr;
+        SL::NET::sockaddr ListenSocketAddr;
 
         Listener(Context *context, std::shared_ptr<ISocket> &&socket, const sockaddr &addr);
         virtual ~Listener();
